8faf6c668e5af08a76082a99d920ca75be22f88f
mr-ekansh <67186634+mr-ekansh@users.noreply.github.com>
2026-04-15 16:37:04 +0530

    B2B error changes

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -203,10 +203,21 @@ public class SocketIOManager : MonoBehaviour
 
   private void OnError(Error err)
   {
-    Debug.LogError("Socket Error Message: " + err);
+    Debug.LogError("[ERROR] Socket error: " + err);
+    if (!string.IsNullOrEmpty(err.message) && err.message.Contains("Session expired"))
+    {
+      Debug.LogWarning("Session expired detected");
+      OnDisconnected();
+#if UNITY_WEBGL && !UNITY_EDITOR
+    JSManager.SendCustomMessage("session_expired");
+#endif
+    }
+    else
+    {
 #if UNITY_WEBGL && !UNITY_EDITOR
     JSManager.SendCustomMessage("error");
 #endif
+    }
   }
 
   void OnResult(string data)
